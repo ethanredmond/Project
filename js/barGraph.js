b1b23c33596e53b4cf237b7cd3aa1874f8ba8d1a
Ethan <ethan.redmond@centre.edu>
2020-11-22 13:08:48 -0500

    final

--- js/barGraph.js
+++ js/barGraph.js
@@ -1,16 +1,16 @@
-var  margin = {top: 30, right: 30, bottom: 70, left: 60};
+var  margin = {top: 30, right: 30, bottom: 120, left: 50};
 var  width = 460 - margin.left - margin.right;
 var  height = 400 - margin.top - margin.bottom;
 
 
 
 
-var graph1 = function(data)   {
+var graph2 = function(data1)   {
 
 // append the svg object to the body of the page
-var svg = d3.select("#graph")
+var svg = d3.select("#barGraph")
   .append("svg")
-    .attr("id", "theGraph")
+    .attr("id", "Graph1")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   svg.append("g")
@@ -20,7 +20,7 @@ var svg = d3.select("#graph")
 // X axis
 var x = d3.scaleBand()
   .range([ 0, width ])
-  .domain(data.map(function(d) { return d.Period; }))
+  .domain(data1.map(function(d) { return d.education; }))
   .padding(0.2);
 svg.append("g")
   .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
@@ -31,7 +31,7 @@ svg.append("g")
 
 // Add Y axis
 var y = d3.scaleLinear()
-  .domain([0, 20])
+  .domain([0, 10])
   .range([ height, 0]);
 svg.append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
@@ -39,17 +39,18 @@ svg.append("g")
 
 // Bars
 svg.selectAll("rect")
-  .data(data)
+  .data(data1)
   .enter()
   .append("rect")
-    .attr("x", function(d)  { return x(d.Period); })  
-    .attr("y", function(d) { return y(d.unemployment_rate); })
+    .attr("x", function(d)  { return x(d.education); })  
+    .attr("y", function(d) { return y(d.rate); })
     .attr("width", x.bandwidth())
-    .attr("height", function(d) { return height - y(parseFloat(d.unemployment_rate)); 
+    .attr("height", function(d) { return height - y(parseFloat(d.rate)); 
     
     })
     .attr("fill", "blue")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
 
     //tool tip
     .on("mouseenter" ,function(rate)
@@ -64,7 +65,7 @@ svg.selectAll("rect")
         .style("left",xPos+"px")
         
         d3.select("#tooltip")
-        .text(rate.unemployment_rate+"%");
+        .text(rate.rate+"%");
         
 
       })//tool tip off
@@ -74,46 +75,48 @@ svg.selectAll("rect")
         .classed("hidden",true);
     })
     
-    
-    
-    drawLabels(margin, width, height);
+    drawLabels1(margin, width, height);
 };
 
-var drawLabels = function(margin, width, height)
+var drawLabels1 = function(margin, width, height)
 {
-var labels = d3.select("#theGraph")
+var labels = d3.select("#Graph1")
 .append("g")
 .classed("labels", true)
 
 labels.append("text")
-.text("Unemployment Rate during COVID")
+.text("Unemployment Rates by Educational Attainment")
 .classed("title", true)
 .attr("text-anchor", "middle")
 .attr("x", margin.left+(width/2))
 .attr("y", margin.top)
 
 labels.append("text")
-.text("Months(2019-2020)")
+.text("Education Attained")
 .classed("label", true)
 .attr("text-anchor", "middle")
-.attr("x", margin.left+(width/2))
-.attr("y", height+(margin.top+40))
+.attr("x", margin.left+(width/2-20))
+.attr("y", height+(margin.top+110))
         
 labels.append("g")
 .attr("transform","translate(10,"+ (margin.top+(height/2))+")")
 .append("text")
-.text("Unemployment Rate(%)")
+.text("Unemployment Rate")
 .classed("label",true)
 .attr("text-anchor","middle")
 .attr("transform","rotate(90)")
 
 
-};
+}
 
-var successFCN = function(data)
+
+
+
+var succesFCN = function(data1)
 {
-    console.log("data collected", data)
-    graph1(data);
+    console.log("education data", data1)
+    graph2(data1);
+    
 }
 
 var failFCN = function(error)
@@ -121,6 +124,6 @@ var failFCN = function(error)
     console.log("error", error);
 }
 
-var barPromise = d3.csv("../csv/SeriesReport3.csv")
+var barPromise1 = d3.csv("../csv/bar2.csv")
 
-barPromise.then(successFCN, failFCN)
+barPromise1.then(succesFCN, failFCN)
